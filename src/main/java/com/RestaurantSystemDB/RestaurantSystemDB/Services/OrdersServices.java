2daf212f89e1b9451b942be463442c9970c8db2b
noorkobari <noor.kobari9@gmail.com>
2023-03-08 09:35:40 +0200

    no message

--- src/main/java/com/RestaurantSystemDB/RestaurantSystemDB/Services/OrdersServices.java
+++ src/main/java/com/RestaurantSystemDB/RestaurantSystemDB/Services/OrdersServices.java
@@ -1,13 +1,12 @@
 package com.RestaurantSystemDB.RestaurantSystemDB.Services;
-import com.RestaurantSystemDB.RestaurantSystemDB.Exceptions.CategoryNotFoundException;
+
 import com.RestaurantSystemDB.RestaurantSystemDB.Exceptions.OrderNotFoundException;
-import com.RestaurantSystemDB.RestaurantSystemDB.Models.Categories;
-import com.RestaurantSystemDB.RestaurantSystemDB.Models.Items;
 import com.RestaurantSystemDB.RestaurantSystemDB.Models.OrderDetails;
 import com.RestaurantSystemDB.RestaurantSystemDB.Models.Orders;
 import com.RestaurantSystemDB.RestaurantSystemDB.Repositories.OrdersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -16,12 +15,12 @@ public class OrdersServices {
     private final OrdersRepository ordersRepository;
 
     @Autowired
-    public OrdersServices(OrdersRepository ordersRepository){
-        this.ordersRepository=ordersRepository;
+    public OrdersServices(OrdersRepository ordersRepository) {
+        this.ordersRepository = ordersRepository;
     }
 
-    public Orders addOrder (Orders order){
-        return  ordersRepository.save(order);
+    public Orders addOrder(Orders order) {
+        return ordersRepository.save(order);
 
     }
 
@@ -34,19 +33,20 @@ public class OrdersServices {
         return updatedOrder;
     }
 
-    public void deleteOrder(Long id){
+    public void deleteOrder(Long id) {
         ordersRepository.deleteOrderById(id);
     }
 
-    public List<Orders> findAllOrders(){
+    public List<Orders> findAllOrders() {
         return ordersRepository.findAll();
     }
 
-    public Orders findOrderById(Long id ){
-        return ordersRepository.findOrderById(id).orElseThrow(()->new OrderNotFoundException("Order with this id "+id+"does not exist"));
+    public Orders findOrderById(Long id) {
+        return ordersRepository.findOrderById(id).orElseThrow(() -> new OrderNotFoundException("Order with this id " + id + "does not exist"));
 
 
     }
+
     public List<OrderDetails> findDetailsByOrdersID(Long orderID) {
         Orders order = ordersRepository.findById(orderID)
                 .orElseThrow(() -> new OrderNotFoundException("DETAILS with this id " + orderID + " does not exist"));
